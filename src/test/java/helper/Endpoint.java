166cf4a21062424ea94c1154d7f8c0d3f4793acd
Ernigee <erni.102299@gmail.com>
2024-10-30 12:19:48 +0700

    ganti URL invalid di endpoint

--- src/test/java/helper/Endpoint.java
+++ src/test/java/helper/Endpoint.java
@@ -5,7 +5,7 @@ public class Endpoint {
     public static final String LIST_USERS_REQRESS = host_reqress + "/api/users?page=2";
     public static final String CREATE_NEW_USER = host_reqress + "/api/users";
     public static final String DELETE_USER = host_reqress + "/api/users/2";
-    public static final String Invalid_URL = host_reqress + "/api/users?page=3";
+    public static final String Invalid_URL = host_reqress + "/api/unknown/23";
 
 
 }
